3c5c50e29a929719080405dbd52cc657b36bd277
hideki <h.itakura@yahoo.com>
2011-01-24 01:53:08 -0800

    delete

--- python/twitter/utils.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-def normalize(text):
-	if text == None or text == "":
-		return ""
-	return text.encode('utf-8').replace("\t", " ").replace("\r\n", " ").replace("\n", " ").replace("\r", " ") 
